6cfd2031a8216ec7c36cf848a77d1efe94cac9a2
Hyunin Jeon <kong111312@gmail.com>
2022-09-27 23:39:49 +0900

    weather

--- e2e/practice.cy.js
+++ e2e/practice.cy.js
@@ -1,9 +1,17 @@
-describe('empty spec', () => {
-  it('click news', () => {
-    cy.visit('https://naver.com')
-      .get('nave_item')
-      .click();
+const { eq } = require("lodash");
 
-    
+describe('empty spec', () => {
+  it('today weather', () => {
+    const content = '오늘 날씨';
+
+    cy.visit('https://naver.com')
+      .get('div')
+      .should('have.class','green_window')
+      .get('input[name=query]')
+      .click()
+      .type(content)
+      .get('button[id=search_btn]')
+      .click()
+
   })
 })
